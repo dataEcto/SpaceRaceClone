0a4262ffedbffa95aa62c1b66a90f3c5f19000c6
Genric <gj.elazegui@Gmail.com>
2019-09-07 14:07:54 -0400

    Space Race Clone 75% Finished
    Everything but the Projectiles that hit the player is done. I predict that the Projectiles are either gonna be A) Tedious and/or B) Difficult. However, once I get it done, the modding comes next.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Public Variables")]
+    public float speed;
+    public GameObject respawnPoint;
+    public float playerOneScore;
+    public Text p1Text;
+
+
+    [Header("Variables for Reference")]
+    public bool p1Scored;
+    public bool shouldRespawn;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+        
+   
+    void Start()
+    {
+        playerOneScore = 0f;
+        p1Scored = false;
+        shouldRespawn = false;
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    
+    void Update()
+    {
+        Vector2 MoveInput = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = MoveInput.normalized * speed;
+        
+        
+
+        if (p1Scored)
+        {
+            playerOneScore = playerOneScore + 1;
+            p1Text.text = playerOneScore.ToString();
+            p1Scored = false;
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+       
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+   
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("I have hit the Trigger");
+        this.transform.position = respawnPoint.transform.position;
+        p1Scored = true;
+    }
+
+}
