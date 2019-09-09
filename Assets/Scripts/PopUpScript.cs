5755104b997f999150bbb49e56112817fd4cb8fb
Genric <gj.elazegui@Gmail.com>
2019-09-09 12:41:12 -0400

    Clone Finished, Add on Complete. Prototype 1 on the Way
    The clone is finished up, as well as the prototype of the Mod. All I need to do is polish up the projectile placement, the Pop Ups size, and spawn points for some objects.
    
    Another thing I should do is make a seperate scene thats just the regular space race, without any gimmicks. This will make playtesting easier for people who havent played Space Race by showing them the original game.

---
+++ Assets/Scripts/PopUpScript.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpScript : MonoBehaviour
+{
+    [Header("Click Functions")]
+    private float startPosX;
+    private float startPosY;
+    public bool isBeingHeld = false;
+
+    [Header("Random Fuctions")]
+    private Vector2 min;
+    private Vector2 max;
+    private float xPos;
+    private float yPos;
+    public Vector2 newPosition;
+    
+    [Header("Pop Up Functions")]
+    private bool shouldPopUp;
+    private bool stayOnScreen;    
+    public float MaxPopUpTimer;
+    public float currentPopUpTimer;
+    private Vector3 originalScale;
+    
+    private bool isScaling;
+    
+    
+    
+    void Start()
+    {
+       SetRanges();
+       shouldPopUp = false;
+       stayOnScreen = false;
+       currentPopUpTimer = MaxPopUpTimer;
+       originalScale = gameObject.transform.localScale;
+       
+       isScaling = false;
+
+    }
+
+    
+    void Update()
+    {
+        xPos = Random.Range(min.x, max.x);
+        yPos = Random.Range(min.y, max.y);
+        
+        if (stayOnScreen == false)
+        {
+            currentPopUpTimer = currentPopUpTimer - 0.5f * Time.deltaTime;
+        
+        }
+        
+        //Code that randomly sets the Ad to appear on screen
+        if (currentPopUpTimer <= 0)
+        {
+            shouldPopUp = true;
+            stayOnScreen = true;
+            currentPopUpTimer = MaxPopUpTimer;
+            gameObject.transform.localScale = originalScale;
+
+        }
+
+        if (shouldPopUp)
+        {
+            newPosition = new Vector2(xPos,yPos);
+            this.gameObject.transform.position = newPosition;
+            shouldPopUp = false;
+        }
+        
+        //Code that moves the pop up ad.
+        if (isBeingHeld == true)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            //Convert screen point of mouse to be exclusive to the game world
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            //Code that changes its transform when being dragged by Mouse
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX,mousePos.y - startPosY,0);
+            //Decrease time and size once clicked on.
+            stayOnScreen = false;
+            StartCoroutine(scaleOverTime(gameObject.transform, new Vector3(0, 0, 0), 2f));
+            
+        }
+
+    }
+    
+    private void SetRanges()
+    {
+        min = new Vector2(-7, -4); //Random value.
+        max = new Vector2(8, 4); //Another ramdon value, just for the example.
+    }
+
+    
+
+    IEnumerator scaleOverTime(Transform objectToScale, Vector3 toScale, float duration)
+    {
+        //Make sure there is only one instance of this function running
+        if (isScaling)
+        {
+            yield break; //exit if this is still running
+        }
+        isScaling = true;
+
+        float counter = 0;
+
+        //Get the current scale of the object to be moved
+        Vector3 startScaleSize = objectToScale.localScale;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            objectToScale.localScale = Vector3.Lerp(startScaleSize, toScale, counter / duration);
+            yield return null;
+        }
+
+        isScaling = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            //Convert screen point of mouse to be exclusive to the game world
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;  
+        }
+       
+    }
+
+    private void OnMouseUp()
+    {
+        
+        isBeingHeld = false;
+    }
+}
---
+++ Assets/Scripts/TimerBar.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerBar : MonoBehaviour
+{
+    public float CurrentTime;
+    public float MaxTime;
+    
+    GameObject playerOne; 
+    GameObject playerTwo; 
+    
+    private PlayerController p1Script;
+    private P2Controller p2Script;
+    
+    private Transform p1Transform;
+    private Transform p2Transform;
+    private Transform respawn1;
+    private Transform respawn2;
+    
+
+    public Slider timer;
+    
+    void Start()
+    {
+        MaxTime = 20;
+        //On game load, set timer to Full
+        CurrentTime = MaxTime;
+        timer.value = CalculateTime();
+        
+        playerOne = GameObject.Find("Spaceship");
+        playerTwo = GameObject.Find("Spaceship 2");
+        
+        p1Script = playerOne.GetComponent<PlayerController>();
+        p2Script = playerTwo.GetComponent<P2Controller>();
+        
+        p1Transform = playerOne.GetComponent<Transform>();
+        p2Transform = playerTwo.GetComponent<Transform>();
+
+        respawn1 = p1Script.respawnPoint.transform;
+        respawn2 = p2Script.respawnPoint2.transform;
+    }
+
+    
+    void Update()
+    {
+        //This is where the timer should go down
+        DecreaseTimer(1f);
+        //If the timer runs out, everything should stop
+       
+    }
+
+    void DecreaseTimer(float decreaseValue)
+    {
+        //Subtract time from the timer bar
+        CurrentTime -= decreaseValue * Time.fixedDeltaTime;
+        timer.value = CalculateTime();
+        if (CurrentTime <= 0)
+        {
+            //Function that stops everything here.
+            Debug.Log("GAME!");
+
+            p1Script.transform.position = respawn1.transform.position;
+            p2Script.transform.position = respawn2.transform.position;
+            p1Script.enabled = false;
+            p2Script.enabled = false;
+        }
+      
+    }
+    
+    
+
+    float CalculateTime()
+    {
+        return CurrentTime / MaxTime;
+    }
+}
